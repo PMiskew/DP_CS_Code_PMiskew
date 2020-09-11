09cf065504422e650240f6d2b90af3f8943d69a5
PMiskew <paul.miskew@gmail.com>
2020-09-11 13:30:31 -0400

    update

---
+++ Demo_Python/DivMod10.py
@@ -0,0 +1,20 @@
+#Div mod 10 algorithm
+'''
+Theory:
+
+The mod operator (%) will give the remainder when division is performed
+14 % 3 = 2
+
+Why?  14/3 = 4R2, mod (%) simpley gives the remainder
+
+Integer Division is what happens when two integers are divided. When
+we integer divide we chop off decimal values. 
+
+In python 3/4 = 0.75, however, in Java (and may other languages) 3/4 = 0. 
+
+Why?  This is becasuse Java see 3 as and an integer and 4 as an integer
+and assumes the result should be an integer so it integer divides.  Python
+however tries to be helpful and thinks you want the decimals so leaves them
+
+In Python // is integer divid
+'''
