90683fe42efc36a2c2cbeccbba6a6af9531cc24d
PMiskew <paul.miskew@gmail.com>
2020-09-14 16:34:54 -0400

    update

---
+++ Demo_Python/async3.py
@@ -0,0 +1,18 @@
+'''
+Big Ideas:  
+
+Reference Types
+
+'''
+
+def scaleElementsA(a, l):
+    print("Scale Elements A")
+
+
+def scaleElementsB(a,l):
+    print("Scale Elements B")
+
+def addStringsSmallLarge(a, b):
+    print("Add Strings Small Large")
+
+    
