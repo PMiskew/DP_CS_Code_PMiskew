125898783549186ed4d3057dc50078ff26116290
Paul Miskew <pmiskew@C1MWP4S2J1WL.local>
2021-01-29 10:43:54 -0500

    update

--- Fake_Login_Demo_Option2/auth.js
+++ Fake_Login_Demo_Option2/auth.js
@@ -8,7 +8,7 @@
 //If you authenticate using firebase your browser gets "authenticated" this means
 //when a page is loaded you run a function that checks to see if the person is valid
 //and if they are it updates the page. 
-
+console.log("TEST")
 
 const login_form = document.querySelector("#login-form")
 const login_nav = document.getElementById("login_nav")
@@ -16,46 +16,54 @@ const logout_nav = document.getElementById("logout_nav")
 const the_elements_nav = document.getElementById("the_elements_nav")
 const learn_more_nav = document.getElementById("learn_more_nav")
 
-login_form.addEventListener('submit',(e) => {
-	e.preventDefault() //stops page from reloading
+try {
 	
-	email = login_form["user_name"].value
-	password = login_form["user_password"].value
-	console.log(email)
-	console.log(password)
+	login_form.addEventListener('submit',(e) => {
+		e.preventDefault() //stops page from reloading
+		
+		email = login_form["user_name"].value
+		password = login_form["user_password"].value
+		console.log(email)
+		console.log(password)
 
-	//Option 1: Verify against a predefined list - for learning
+		//Option 1: Verify against a predefined list - for learning
 
-	login_nav.style.display = "none"
-	logout_nav.style.display = "block"
-	the_elements_nav.style.display = "block"
-	learn_more_nav.style.display = "block"
-
-
-	//Option 2: Send to Firebase for authorization - for real. 
-	auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
-	
-    	var user = userCredential.user;
-  	
-
-  		login_nav.style.display = "none"
+		login_nav.style.display = "none"
 		logout_nav.style.display = "block"
 		the_elements_nav.style.display = "block"
 		learn_more_nav.style.display = "block"
 
-  	
-  	}).catch((error) => {
-    	console.log('ERROR')
-    	var errorCode = error.code;
-    	var errorMessage = error.message;
-  	});
 
-	//General
-	const modal = document.querySelector('#login_modal'); 
-	M.Modal.getInstance(modal).close();
-	login_form.reset()
+		//Option 2: Send to Firebase for authorization - for real. 
+		auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
+		
+	    	var user = userCredential.user;
+	  	
 
-});
+	  		login_nav.style.display = "none"
+			logout_nav.style.display = "block"
+			the_elements_nav.style.display = "block"
+			learn_more_nav.style.display = "block"
+
+	  	
+	  	}).catch((error) => {
+	    	console.log('ERROR')
+	    	var errorCode = error.code;
+	    	var errorMessage = error.message;
+	  	});
+
+		//General
+		const modal = document.querySelector('#login_modal'); 
+		M.Modal.getInstance(modal).close();
+		login_form.reset()
+
+	});
+
+}
+catch(error) {
+	console.log("No form")
+}
+
 
 logout_nav.addEventListener('click',(e) => {
 
@@ -65,34 +73,34 @@ logout_nav.addEventListener('click',(e) => {
 	learn_more_nav.style.display = "none"
 	
 	firebase.auth().signOut()
-
+	window.open("index.html","_self");
 
 });
 
 
+
 //****************************************************
 auth.onAuthStateChanged(user => {
-	console.log("***",user)
-    /*
-    if (user) {
-    	console.log("LOGGED IN")
-        user.getIdTokenResult().then(idTokenResult => {
-            console.log(idTokenResult.claims.admin);
+	console.log("^^^^^^",user)
 
-        });
-        db.collection('guides').onSnapshot(snapshot => {
-            setupGuides(snapshot.docs)
-            setupUI(user)
-        }, err => {
-            console.log(err.message)
 
-        });
-    }
-    else {
-        console.log("NO LOGGED IN")
-        
-    }
-    */
+	if (user === null) {
+
+		login_nav.style.display = "block"
+		logout_nav.style.display = "none"
+		the_elements_nav.style.display = "none"
+		learn_more_nav.style.display = "none"
+	}
+	else {
+
+  		login_nav.style.display = "none"
+		logout_nav.style.display = "block"
+		the_elements_nav.style.display = "block"
+		learn_more_nav.style.display = "block"
+
+	}
+
+
 });
 
 
